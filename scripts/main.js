93490fb6827c57437dae97b22c1a57075ff5c67e
Bugin10 <ben.axford@hotmail.com>
2016-11-18 16:46:26 +1030

    Generate random ground Some refactoring

--- scripts/main.js
+++ scripts/main.js
@@ -24,7 +24,7 @@ var render = Render.create({
         showAngleIndicator: true,
         showShadows: true,
         wireframes: false,
-        width: 1000,
+        width: 1400,
         height: 800,
         hasBounds: true,
         showBounds: true,
@@ -47,61 +47,49 @@ engine.world.bounds.max.y = 100000;
 
 World.add(engine.world, mouseConstraint);
 // create two boxes and a ground
-var boxA = Bodies.circle(400, 200, 80, 80);
+var boxA = Bodies.circle(200, 200, 80, 80);
 boxA.friction = 0.15;
-var boxB = Bodies.circle(200, 200, 80, 80);
+var boxB = Bodies.circle(400, 200, 80, 80);
 boxB.friction = 0.15;
-var ground = Bodies.rectangle(200, 400, 400, 30,
-    { isStatic: true, angle: Math.PI * 0.0, friction: 1, chamfer: { radius: 15 }, restitution: 0 });
+var ground = Bodies.rectangle(200, 400, 80, 20,
+    { isStatic: true, angle: Math.PI * 0.0, friction: 1, chamfer: { radius: 9 }, restitution: 0 });
 
-var rand = Math.random() * 0.5 + 0.25;
+var rand = Math.random() * ((1 + 0.001) / 500.0) + (0.5 - (1 / 1000));
 
-if (rand < 0.5) {
-    var offset = 1 - rand;
-}
-else {
-    var offset = rand
-}
-
-if (rand > 0.5) {
-    var yoffset = rand;
-}
 var groundComp = Composite.create();
 var groundComp2 = Composite.create();
-var ground1 = Bodies.rectangle(ground.bounds.max.x + 180, ground.position.y, 400, 30,
-    { isStatic: true, friction: 1, chamfer: { radius: 15 } });
-Composite.add(groundComp, ground1);
+// var ground1 = Bodies.rectangle(ground.bounds.max.x + 30, ground.position.y + 15, 80, 20,
+//     { isStatic: true, friction: 1, chamfer: { radius: 9 } });
+// Composite.add(groundComp, ground1);
 
-Composite.rotate(groundComp, -Math.PI * rand + (Math.PI / 2), { x: ground1.bounds.min.x, y: ground1.position.y });
+//Composite.rotate(groundComp, -Math.PI * rand + (Math.PI / 2), { x: ground1.bounds.min.x, y: ground1.position.y });
 
 
 if (rand < 0.5) {
-    var pastGroundY = ground1.bounds.max.y - 15;
+    var pastGroundY = ground.bounds.max.y - 10;
 }
 else {
-    var pastGroundY = ground1.bounds.min.y + 15;
+    var pastGroundY = ground.bounds.min.y + 10;
 }
-var previousTileBounds = Vector.create(ground1.bounds.max.x, pastGroundY);
+var previousTileBounds = Vector.create(ground.bounds.max.x, pastGroundY);
 
 console.log(previousTileBounds);
 console.log("ok");
 
 var average = 0;
 floorTiles = [groundComp2];
-for (i = 0; i < 100; i++) {
+for (i = 0; i < 1000; i++) {
 
     floorTiles[i] = Composite.create();
-    var nextGroundTile = Bodies.rectangle(previousTileBounds.x + 90, previousTileBounds.y, 200, 20,
+    var nextGroundTile = Bodies.rectangle(previousTileBounds.x + 30, previousTileBounds.y, 80 , 20,
         { isStatic: true, friction: 1, chamfer: { radius: 9 } });
     Composite.add(floorTiles[i], nextGroundTile);
 
-    var rand = Math.random()* ((i + 0.001)/50.0) + (0.5-(i/100));
-    if(rand>1)
-    {
+    var rand = Math.random() * ((i + 0.001) / 500.0) + (0.5 - (i / 1000));
+    if (rand > 1) {
         rand = 0.9;
     }
-    if(rand<0)
-    {
+    if (rand < 0) {
         rand = 0.1;
     }
     Composite.rotate(floorTiles[i], -Math.PI * rand + (Math.PI / 2), { x: nextGroundTile.bounds.min.x, y: nextGroundTile.position.y });
@@ -114,10 +102,10 @@ for (i = 0; i < 100; i++) {
     }
     previousTileBounds.x = nextGroundTile.bounds.max.x;
     previousTileBounds.y = pastGroundY;
-   average += rand;
+    average += rand;
     World.add(engine.world, floorTiles[i]);
 }
-console.log(average/100);
+console.log(average / 100);
 
 
 World.add(engine.world, Constraint.create({ bodyA: boxA, bodyB: boxB }));
@@ -159,38 +147,47 @@ function Minimap() {
     ctx.fill();
 
     ctx.fillStyle = '#aaf';
-    ctx.fillRect(boxA.position.x / floorTiles[99].bodies[0].bounds.max.x * 320,  (boxB.position.y/25 + 125), 5, 5);
+    ctx.fillRect(boxA.position.x / floorTiles[999].bodies[0].bounds.max.x * 320, (boxB.position.y / 50 + 125), 5, 5);
     ctx.fillStyle = 'orange';
-    for(i =0; i<100; i++)
-    {
-        if(floorTiles[i].bodies[0].angle > Math.pi)
-        {
-            var tiley = floorTiles[i].bodies[0].bounds.max.y;
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.strokeStyle = 'yellow';
+    // ctx.moveTo(floorTiles[0].bodies[0].bounds.min.x,floorTiles[0].bodies[0].bounds.min.y );
+
+    for (i = 0; i < 1000; i++) {
+        if (floorTiles[i].bodies[0].angle > Math.pi) {
+            var tileRighty = floorTiles[i].bodies[0].bounds.max.y;
+            var tileLefty = floorTiles[i].bodies[0].bounds.min.y;
         }
-        else
-        {
-            var tiley = floorTiles[i].bodies[0].bounds.min.y;
+        else {
+            var tileRighty = floorTiles[i].bodies[0].bounds.min.y;
+            var tileLefty = floorTiles[i].bodies[0].bounds.max.y;
         }
-        ctx.fillRect(floorTiles[i].bodies[0].position.x/floorTiles[99].bodies[0].bounds.max.x * 320, tiley/25 + 125, 3, 3);
+
+        ctx.lineTo(floorTiles[i].bodies[0].bounds.min.x / floorTiles[999].bodies[0].bounds.max.x * 320, floorTiles[i].bodies[0].bounds.min.y / 50 + 125);
+        // ctx.fillRect(floorTiles[i].bodies[0].position.x / floorTiles[99].bodies[0].bounds.max.x * 320, tileRighty / 25 + 125, 3, 3);
     }
+    ctx.stroke();
 }
 
 
 
 
 (function run() {
+    +
     window.requestAnimationFrame(run);
     Body.setAngularVelocity(boxA, Math.PI * 0.04);
     Body.setAngularVelocity(boxB, Math.PI * 0.04);
 
     //
     // var hero = boxA;
-    render.bounds.min.x = boxA.bounds.min.x - 500;
-    render.bounds.max.x = boxA.bounds.min.x + 500;
+    render.bounds.min.x = boxA.bounds.min.x - 700;
+    render.bounds.max.x = boxA.bounds.min.x + 700;
 
     render.bounds.min.y = boxA.bounds.min.y - 400;
     render.bounds.max.y = boxA.bounds.min.y + 400;
-   // console.log(boxA.position.x);
+    // console.log(boxA.position.x);
     // // Update Mouse
     Mouse.setOffset(mouseConstraint.mouse, render.bounds.min);
     Minimap();
